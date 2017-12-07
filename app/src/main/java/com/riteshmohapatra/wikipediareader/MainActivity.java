cbeb38b9e1358397aaf6295b7d9e587e6fc5de2a
RITESH KUMAR MOHAPATRA <mohapatraritesh97@gmail.com>
2017-12-07 10:17:36 +0530

    Update (#6)
    * Toolbar Search added.
    
    * Implements search in Action Bar.
    
    * Prevents fab from blocking content. (#7)
    
    * UI Enhancements (#8)
    
    * Prevents fab from blocking content.
    
    * Code cleanup
    
    * Code cleanup #2
    
    * Entire article is displayed. (#10)
    
    * Prevents fab from blocking content.
    
    * Code cleanup
    
    * Code cleanup #2
    
    * Displaying full content now
    
    * Can speak full content now
    
    * Basic image support (#11)
    
    * Prevents fab from blocking content.
    
    * Rudimentary image support added.
    
    * Adds the ability to search. (#12)
    
    * Prevents fab from blocking content.
    
    * Added search support.

--- app/src/main/java/com/riteshmohapatra/wikipediareader/MainActivity.java
+++ app/src/main/java/com/riteshmohapatra/wikipediareader/MainActivity.java
@@ -6,6 +6,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -26,12 +37,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +54,18 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
     private TextToSpeech tts;
 
+    // viewer
+    private TextView textView;
+    private ImageView imageView;
+    private ProgressBar progress;
+    private View textViewer;
+
+    // results
+    private ListView resultsList;
+    private List<String> results = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
+    private FloatingActionButton fab;
     // search
     private Toolbar toolbar;
     private MenuItem searchBtn;
@@ -68,6 +95,24 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize the views
         textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.image);
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+        textViewer = findViewById(R.id.textViewer);
+        resultsList = (ListView) findViewById(R.id.results);
+        fab = (FloatingActionButton) findViewById(R.id.volume);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); // set toolbar as the ActionBar
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,results);
+        resultsList.setAdapter(adapter);
+        resultsList.setItemsCanFocus(false);
+        resultsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                display(results.get(position));
+            }
+        });
         progress = (ProgressBar) findViewById(R.id.progressBar);
         textViewer = findViewById(R.id.textViewer);
         fab = (FloatingActionButton) findViewById(R.id.volume);
@@ -85,7 +130,14 @@ public class MainActivity extends AppCompatActivity {
                     fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_play_arrow));
                 } else {
                     fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_stop));
-                    tts.speak(textView.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+                    // flush the tts queue
+                    tts.speak(" ", TextToSpeech.QUEUE_FLUSH, null);
+
+                    // Divide string into chunks
+                    String article = textView.getText().toString();
+                    for (int index = 0; index < article.length(); index += 3000)
+                        tts.speak(article.substring(index, Math.min(index + 3000,article.length())),
+                                  TextToSpeech.QUEUE_ADD, null); // add chunk to queue
                 }
             }
         });
@@ -113,6 +165,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
+
+        final MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView)searchMenuItem.getActionView();
+        searchView.setQueryHint("Search Wikipedia");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override       // what happens when the user submits a query
+            public boolean onQueryTextSubmit(String query) {
+                searchMenuItem.collapseActionView(); // collapse the search box
+                if (tts.isSpeaking()) {     // if tts is speaking, stop it
+                    tts.stop();
+                    fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_play_arrow));
+                }
+                search(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // do nothing
+                return false;
+            }
+        });
+
+        searchView.setOnQueryTextFocusChangeListener(new OnFocusChangeListener() {
+            @Override       // hide the search box if the user focuses on something else
+            public void onFocusChange(View view, boolean queryTextFocused) {
+                if(!queryTextFocused) {
+                    searchView.setIconified(true);
+                    searchMenuItem.collapseActionView();
+                }
+            }
+        });
+
+        return true;
         return true;
     }
 
@@ -131,6 +217,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        if (results.size() > 0 && resultsList.getVisibility() != View.VISIBLE) {
+            setTitle("Search Results");
+            textViewer.setVisibility(View.INVISIBLE);
+            resultsList.setVisibility(View.VISIBLE);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void search(String query) {
+        String url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srprop=&format=json&srsearch=" + encodeURIComponent(query);
         if(isSearchOpen)
             hideSearchBox();
         else
@@ -146,6 +243,12 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(JSONObject response) {       // response received
+                        progress.setVisibility(View.INVISIBLE);
+                        try {
+                            JSONArray jsonresults = response.getJSONObject("query").getJSONArray("search");
+                            for (int i=0; i<jsonresults.length(); i++)
+                                results.add(jsonresults.getJSONObject(i).getString("title"));
+                            adapter.notifyDataSetChanged();
                         progress.setVisibility(View.INVISIBLE);         // hide the progress bar
                         try {
                             JSONObject pages = response.getJSONObject("query")
@@ -163,6 +266,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {        // no response
+                        //progress.setVisibility(View.INVISIBLE);
                         progress.setVisibility(View.INVISIBLE);
                         // todo: view image
                         Toast.makeText(MainActivity.this,"Error in getting response", Toast.LENGTH_SHORT).show();
@@ -174,6 +278,60 @@ public class MainActivity extends AppCompatActivity {
         // Add the request to the RequestQueue.
         textViewer.setVisibility(View.INVISIBLE);
         progress.setVisibility(View.VISIBLE);           // make the progress bar visible
+        results.clear(); adapter.notifyDataSetChanged();
+        resultsList.setVisibility(View.VISIBLE);
+        setTitle("Search Results");
+        MainActivity.this.queue.add(jsObjRequest);
+    }
+
+    private void display(String title) {        // fetches the article and loads it into the viewer.
+        textViewer.requestFocus();
+        String url = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages|extracts&format=json&piprop=thumbnail&pithumbsize=300&titles=" + encodeURIComponent(title);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {       // response received
+                        progress.setVisibility(View.INVISIBLE);         // hide the progress bar
+                        imageView.setVisibility(View.GONE);
+                        try {
+                            JSONObject pages = response.getJSONObject("query").getJSONObject("pages");
+                            String firstPage = pages.keys().next();         // extract the first result
+                            String text = pages.getJSONObject(firstPage).getString("extract");
+                            try {       // not all articles have images
+                                String imgurl = pages.getJSONObject(firstPage).getJSONObject("thumbnail").getString("source");
+                                imageView.setVisibility(View.VISIBLE);
+                                Picasso.with(getApplicationContext()).load(imgurl).placeholder(R.drawable.placeholder).resize(600,0).into(imageView);
+                            } catch (JSONException e) { } // do nothing
+                            textViewer.setVisibility(View.VISIBLE);         // make viewer visible
+                            textView.setText(Html.fromHtml(text));          // load the content into the viewer.
+                        } catch (JSONException ex) {                        // response could not be parsed.
+                            Toast.makeText(MainActivity.this,"Error in parsing response", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {        // no response
+                        progress.setVisibility(View.INVISIBLE);
+                        // todo: view image
+                        Toast.makeText(MainActivity.this,"Error in getting response", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+        // Add the request to the RequestQueue.
+        setTitle(title);
+        textViewer.setVisibility(View.INVISIBLE);
+        progress.setVisibility(View.VISIBLE);           // make the progress bar visible
+        resultsList.setVisibility(View.INVISIBLE);
+        MainActivity.this.queue.add(jsObjRequest);
+    }
+
+    private static String encodeURIComponent(String s) {
+        String result;
         MainActivity.this.queue.add(jsObjRequest);
     }
 
@@ -278,5 +436,4 @@ public class MainActivity extends AppCompatActivity {
 
         return result;
     }
-
 }
